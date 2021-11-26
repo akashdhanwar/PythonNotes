19c1a9b4fc83089ec196ff1341c79bd58bb1663e
akash.dhanwar112@gmail.com <akash.dhanwar112@gmail.com>
2021-11-26 17:02:36 +0530

    Updated File

--- 1. Basics/workingwithFiles.py
+++ 1. Basics/workingwithFiles.py
@@ -1,11 +1,22 @@
 # Reading
 with open("test.txt") as myFile:
-    print(myFile.read())
+    print("1. Reading -\n" + myFile.read())
 
 # Writing
 with open("test.txt","w") as myFile:
     myFile.write("Updated Data")
 
+# Create a new file and then write. If file is already present, it throws error
+# with open("test.txt","x") as myFile:
+    # myFile.write("Updated Data")
+
+
 # Appending
-with open("text.txt","a") as myFile:
-    print()
+with open("test.txt","a") as myFile:
+    myFile.write("\nAppended Data")
+
+# Appending and reading simultaneously
+with open("test.txt","a+") as myFile:
+    myFile.write("\nNew Item Added")
+    myFile.seek(0)
+    print("2. Appending -\n",myFile.read())
