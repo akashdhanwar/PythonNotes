a2788009b8ef9c5d7633b0d12ca6941283a7f3eb
akash.dhanwar112@gmail.com <akash.dhanwar112@gmail.com>
2021-11-27 20:55:21 +0530

    Updated

--- 1. Basics/9. workingWithPandas.py
+++ 1. Basics/9. workingWithPandas.py
@@ -1,1 +1,22 @@
 # pip install ipython
+# ipython
+
+# DataFrames
+import pandas
+df1 = pandas.DataFrame([[1,2,3],[10,20,30]])
+df1 = pandas.DataFrame([[1,2,3],[10,20,30]], columns=(["Price","Age","Value"]), index=["First","Second"]) # Naming columns
+df2 = pandas.DataFrame([{"Name":"Akash","Surname":"Dhanwar"},{"Name":"Jack"}])
+print(df1,"\n")
+print(df2,"\n")
+# dir(df1)
+print(df1.mean(),"\n")
+print(df1.mean().mean(),"\n")
+print(df1.Price,"\n")  # Price column
+print(df1.Price.mean(),"\n")
+print(df1.Price.max(),"\n")  
+
+
+# pip3.10 install jupyter
+# jupyter notebook  OR py -3.9 -m jupyter notebook OR python3.9 -m jupyter notebook
+# Jupytor is used for testing as cmd and saving code parallelly
+
